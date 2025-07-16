5583eee5138c191f787824314b591998b5ba7217
Marcin Ciucmanski <m.ciuciek@o2.pl>
2025-07-16 20:25:59 +0200

    working backend

--- src/main/java/org/sportradar/Game.java
+++ src/main/java/org/sportradar/Game.java
@@ -1,6 +1,7 @@
 package org.sportradar;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
@@ -10,18 +11,20 @@ import java.util.Date;
  */
 
 @Getter
+@Setter
 public class Game {
     private final String homeTeam;
     private final String awayTeam;
     private int homeScore;
     private int awayScore;
-    private final Date created;
+    private final long created;
 
     public Game(String homeTeam, String awayTeam) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.homeScore = 0;
         this.awayScore = 0;
-        this.created = new Date();
+        this.created = System.nanoTime();
+        // Using System.nanoTime() for high precision timestamp so it will be easier to compare recency
     }
 }
--- src/main/java/org/sportradar/ScoreBoard.java
+++ src/main/java/org/sportradar/ScoreBoard.java
@@ -1,7 +1,9 @@
 package org.sportradar;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,25 +14,46 @@ import java.util.List;
 @Getter
 public class ScoreBoard {
 
-    private List<Game> games;
+    private final List<Game> games = new ArrayList<>();
 
     public void startGame(String homeTeam, String awayTeam) {
-        //TODO:  Start a game. When a game starts, it should capture (being initial score 0-0)
-        // a. Home team
-        // b. Away Team
-    }
-
-    public void finishGame(String homeTeam, String awayTeam) {
-        //TODO: Finish a game. It should remove the game from the scoreboard.
+        Game newGame = new Game(homeTeam, awayTeam);
+        if (games.stream().anyMatch(game -> game.getHomeTeam().equals(homeTeam) || game.getAwayTeam().equals(awayTeam))) {
+            // It is possible to not find anything if homeTeam will be set as awayTeam and vice versa,
+            // but we can assume that user is not going to do that.
+            throw new IllegalArgumentException("A game with one of the teams already exists.");
+        }
+        games.add(newGame);
     }
 
     public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-        //TODO: Update score. Receiving the pair score; home team score and away team score updates a game score
+        Game game = games.stream()
+                .filter(g -> g.getHomeTeam().equals(homeTeam) && g.getAwayTeam().equals(awayTeam))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No game found with the specified teams."));
+
+        game.setHomeScore(homeScore);
+        game.setAwayScore(awayScore);
+    }
+
+    public void finishGame(String homeTeam, String awayTeam) {
+        if (!games.removeIf(game -> game.getHomeTeam().equals(homeTeam) && game.getAwayTeam().equals(awayTeam))) {
+            throw new IllegalArgumentException("No game found with the specified teams.");
+        }
     }
 
     public List<Game> getSummary() {
-        //TODO: Get a summary of games by total score. Those games with the same total score
-        // will be returned ordered by the most recently added to our system.
-        return List.of();
+        return games.stream()
+                .sorted((g1, g2) -> {
+                    int scoreComparison = Integer.compare(
+                            g2.getHomeScore() + g2.getAwayScore(),
+                            g1.getHomeScore() + g1.getAwayScore()
+                    );
+                    if (scoreComparison != 0) {
+                        return scoreComparison;
+                    }
+                    return Long.compare(g2.getCreated(), g1.getCreated()); // The more recent game comes first
+                })
+                .toList();
     }
 }
